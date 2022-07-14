f10d066e3469695d7279463c94fe51b0379c0632
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2022-07-15 00:38:35 +0200

    fonts and optimization for webpack were added.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import Post from "./Post"
+import * as $ from 'jquery'
+import Post from "@/Post"
 import json from './assets/json.json'
 import WebpackLogo from './assets/webpack-logo.png'
 import './styles/styles.css'
 
 
 const post = new Post('Webpack Post Title', WebpackLogo)
+
+$('pre').html(post.toString())
 
 console.log('Post to string: ', post.toString())
 
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,22 @@ module.exports = {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist')
     },
+    resolve: {
+        //* if you don't want to write .png after path to the file. (.js and .json are default settings)
+        //extensions: ['.js', '.json', '.png'],
+        //* alias for path. For example:        import Post from '@/Post.js'
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@node_modules': path.resolve(__dirname, 'node_modules')
+        }
+    },
+    optimization: {
+        //*     The optimization for the included libs. For example the jquery module imported two times
+        //*     in the two files (index.js and analytics.js), but in the dist included only one time.
+        splitChunks: {
+            chunks: 'all'
+        }
+    },
     plugins: [
         new HTMLWebpackPlugin({
             template: './index.html'
@@ -31,6 +47,11 @@ module.exports = {
                 test: /\.(png|jpg|jpeg|gif|ico|svg)$/,
                 type: 'asset/resource'
                 //use: ['file-loader']
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                type: 'asset/resource'
+                // use: ['file-loader']
             }
         ]
     }
